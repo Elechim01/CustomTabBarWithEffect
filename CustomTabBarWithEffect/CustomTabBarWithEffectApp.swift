5047746ab5e91ce8384402f3c9600464fbf89653
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-05 22:31:07 +0200

    Initial Commit

---
+++ CustomTabBarWithEffect/CustomTabBarWithEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarWithEffectApp.swift
+//  CustomTabBarWithEffect
+//
+//  Created by Michele Manniello on 05/08/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarWithEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
